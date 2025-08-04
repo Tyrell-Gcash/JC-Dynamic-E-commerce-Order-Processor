42ec433b0f3d86d51b31e6edfd85ece31f9e4a03
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-08-04 16:58:44 +0800

    First part done

--- InteractiveOrderProcessor/src/main/java/org/example/InteractiveOrderProcessor.java
+++ InteractiveOrderProcessor/src/main/java/org/example/InteractiveOrderProcessor.java
@@ -9,10 +9,10 @@ public class InteractiveOrderProcessor {
         //Part 1: Interactive Order Processing Logic
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("--- Enter Order Details ---");
+        System.out.println("\n--- Enter Order Details ---");
 
         System.out.print("Enter unit price: ");
-        double unitPrice = scanner.nextDouble();
+        float unitPrice = scanner.nextFloat();
 
         System.out.print("Enter quantity: ");
         int quantity = scanner.nextInt();
@@ -29,7 +29,7 @@ public class InteractiveOrderProcessor {
         System.out.print("Enter discount code (SAVE10, FREESHIP, or \"\" for none): ");
         String discountCode = scanner.next();
 
-        System.out.println("--- Order Details ---");
+        System.out.println("\n--- Order Details ---");
         System.out.println("Unit Price: $" + unitPrice);
         System.out.println("Quantity: " + quantity);
         System.out.println("is Member: " + hasMembership);
@@ -37,14 +37,74 @@ public class InteractiveOrderProcessor {
         System.out.println("Shipping Zone: " + shippingZone);
         System.out.println("Discount Code: " + discountCode);
 
-        System.out.println("--- Calculation Steps ---");
-        System.out.println("Unit Price: $" + unitPrice);
-        System.out.println("Quantity: " + quantity);
-        System.out.println("is Member: " + hasMembership);
-        System.out.println("Customer Tier: " + customerTier);
-        System.out.println("Shipping Zone: " + shippingZone);
-        System.out.println("Discount Code: " + discountCode);
-        
+
+        float initialSubtotal = unitPrice * quantity;
+
+        System.out.println("\n--- Calculation Steps ---");
+        System.out.printf("Initial Subtotal: $%.2f\n", initialSubtotal);
+
+        //Tier Discount
+        if(customerTier.equalsIgnoreCase("gold")){
+            initialSubtotal *= 0.85f;
+            System.out.printf("After Tier Discount (Gold - 15%%): $%.2f\n", initialSubtotal);
+        }
+        else if (customerTier.equalsIgnoreCase("silver")) {
+            initialSubtotal *= 0.90f;
+            System.out.printf("After Tier Discount (Silver - 10%%): $%.2f\n", initialSubtotal);
+        }
+        else {
+            System.out.printf("After Tier Discount (None): $%.2f\n", initialSubtotal);
+        }
+
+        //Quantity Discount
+        if(quantity >= 5) {
+            initialSubtotal *= 0.95f;
+            System.out.printf("After Quantity Discount (5%% for >=5 items): $%.2f\n", initialSubtotal);
+        }
+
+
+        //Promo Code Discount
+        if(discountCode.equals("SAVE10") && initialSubtotal > 75.00) {
+            initialSubtotal -= 10f;
+            System.out.printf("After Promotional Code (SAVE10 for >$75): $%.2f\n",initialSubtotal);
+        }
+        else if (discountCode.equals("FREESHIP")) {
+            System.out.println("After Promotional Code (FREESHIP for Free Shipping)\n");
+        }
+
+        //Small Order Surcharge
+        initialSubtotal = (initialSubtotal < 25.00) ? initialSubtotal += 3.00: initialSubtotal;
+
+        float shippingCost;
+        if(!discountCode.equals("FREESHIP")) {
+            switch (shippingZone){
+                case "ZoneA":
+                    shippingCost = 5.0f;
+                    System.out.printf("\nShipping Cost: $%.2f (ZoneA)\n", shippingCost);
+                    break;
+                case "ZoneB":
+                    shippingCost = 12.50f;
+                    System.out.printf("\nShipping Cost: $%.2f (ZoneB)\n", shippingCost);
+                    break;
+                case "ZoneC":
+                    shippingCost= 20.0f;
+                    System.out.printf("\nShipping Cost: $%.2f (ZoneC)\n", shippingCost);
+                    break;
+                default:
+                    shippingCost = 25.0f;
+                    System.out.printf("\nShipping Cost: $%.2f (Others)\n", shippingCost);
+                    break;
+            }
+        }
+        else {
+            shippingCost = 0;
+        }
+
+        float finalOrderTotal = initialSubtotal + shippingCost;
+
+        System.out.printf("\nFinal Order Total: $%.2f",finalOrderTotal);
+
+
 //        --- Calculation Steps ---
 //        Initial Subtotal: $240.00
 //        After Tier Discount (Gold - 15%): $204.00
